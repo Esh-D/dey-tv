6a482286248acf6dc659af7ed4008a2f64026222
Eshika Dey <edey@uwaterloo.ca>
2025-02-08 14:14:14 -0500

    added new channels

--- src/dummy-data/data.ts
+++ src/dummy-data/data.ts
@@ -19,9 +19,27 @@ export const CHANNELS = [
     },
     {
         id: 4,
+        channelName: "Somoy TV",
+        picURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRnBKAqSqyVshO_uSNZwAukjzuyWGlO6cGaw&s",
+        link: "https://www.youtube.com/watch?v=4hAzkFtRp70&ab_channel=SOMOYTV"
+    },
+    {
+        id: 5,
         channelName: "CP24",
         picURL: "https://yt3.googleusercontent.com/ytc/AIdro_npxJYcSBW7TsWAHCqHeKXKkS4pk4Tok969x1ZY4lmdKw=s900-c-k-c0x00ffffff-no-rj",
         link: "https://www.cp24.com/video/live/"
+    },
+    {
+        id: 6,
+        channelName: "Netflix",
+        picURL: "https://images.ctfassets.net/4cd45et68cgf/Rx83JoRDMkYNlMC9MKzcB/2b14d5a59fc3937afd3f03191e19502d/Netflix-Symbol.png?w=700&h=456",
+        link: "https://www.netflix.com/ca/"
+    },
+    {
+        id: 7,
+        channelName: "Disney+",
+        picURL: "https://i.insider.com/615202512fb46b0019be4576?width=1200&format=jpeg",
+        link: "https://www.disneyplus.com/en-ca"
     },
 ];
 
